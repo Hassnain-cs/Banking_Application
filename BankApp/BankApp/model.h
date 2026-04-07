7f88e30f69bf29fd7e4606eb1495175f6e374fca
Hassnain-cs <mhassnain3547@conestogac.on.ca>
2026-04-06 20:38:03 -0400

    Extend account creation with structured address, email, and phone inputs

--- BankApp/BankApp/model.h
+++ BankApp/BankApp/model.h
@@ -7,7 +7,10 @@
 #define MAX_NAME        25
 #define MAX_PASSWORD    16
 #define MAX_NICKNAME    16
-#define MAX_ADDRESS     100
+#define MAX_ADDRESS     50
+#define MAX_EMAIL       50
+#define MAX_PHONE       15
+#define MAX_POSTAL      10
 
 typedef enum {
     TX_DEPOSIT = 1,
@@ -26,7 +29,14 @@ typedef struct {
     int  birthMonth;
     int  birthYear;
 
-    char address[MAX_ADDRESS];
+    char streetNumber[10];
+    char streetName[MAX_ADDRESS];
+    char city[MAX_ADDRESS];
+    char country[MAX_ADDRESS];
+    char postalCode[MAX_POSTAL];
+
+    char email[MAX_EMAIL];
+    char phone[MAX_PHONE];
 
     char password[MAX_PASSWORD];
     double balance;
